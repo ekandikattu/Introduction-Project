285c4aefbad7b66050dc860e43b9986329764a4f
driuft <air.socarras@gmail.com>
2022-10-22 23:44:24 -0400

    Update card view UI

--- app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
+++ app/src/main/java/com/driuft/hellosquirrel/MainActivity.kt
@@ -1,26 +1,44 @@
 package com.driuft.hellosquirrel
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.content.res.AppCompatResources
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var profileImage: ImageView
     private lateinit var profileName: TextView
+    private lateinit var profileBio: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Hide action bar
-        supportActionBar?.hide()
-
-        configureProfile()
+        configureViews()
+        setupProfile()
     }
 
-    private fun configureProfile() {
+    private fun configureViews() {
+        profileImage = findViewById(R.id.profile_image)
         profileName = findViewById(R.id.profile_name)
+        profileBio = findViewById(R.id.profile_bio)
+    }
 
+    private fun setupProfile() {
+        configureImage()
+        configureName()
+    }
+
+    private fun configureImage() {
+        profileImage.setImageDrawable(AppCompatResources.getDrawable(this, R.drawable.ic_launcher_foreground))
+    }
+
+    private fun configureName() {
         profileName.text = getString(
             R.string.full_name,
             getString(R.string.first_name),
